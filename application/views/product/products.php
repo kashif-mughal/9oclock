8a0d1b9fbcaac74c6a40aed81da9441eba8f3480
Mumtaz Alam <mumtaz.alam.cs@gmail.com>
2021-02-26 05:10:32 +0500

    Fix: Product Inner Page Quantity Design Issue

--- application/views/product/products.php
+++ application/views/product/products.php
@@ -63,7 +63,6 @@
                                 <div class="featured-products-content mb-2 d-flex justify-content-between">
                                     <div class="card each-prod product-card-inner">
                                         <div class="card-body p-0">
-                                            <a href="{productId}">
                                                 <div class="header">
                                                     {discountString}
                                                     <!--<a href="#" class="add_to_favorite">
@@ -71,7 +70,9 @@
                                                     </a>-->
                                                 </div>
                                             </div>
+                                            <a href="{productId}">
                                             <img class="img-fluid text-center" src="{imgUrl}" alt="Card image cap">
+                                            </a>
                                             <div class="product-info text-center">
                                                 <p class="card-text product-card-inner-name" title="{productName}">{productName}</p>
                                                 <p class="card-text product-card-inner-weight">{unitName}</p>
@@ -94,6 +95,7 @@
                                             data-json="{pjsonString}"
                                             >Remove From Cart</a>
                                     </div>
+
                                 </div>
                             </div>
                             </script>
@@ -129,64 +131,70 @@
                                         echo '<span>'. $value["sort"] .'</span>';
                                     }
                                 ?>
+                                    
                                 <div class="featured-products-content mb-2 d-flex align-items-center justify-content-start">
-                                    <div class="card mr-2 each-prod product-card-inner" style="padding-top:5px;">
-                                        <div class="card-body p-0">
-                                            <a href="<?php echo base_url() . 'Cproduct/viewProduct/' . $value['ProductId']; ?>">    
+                                        <div class="card mr-2 each-prod product-card-inner" style="padding-top:5px;">
+                                            <div class="card-body p-0">
                                                 
-                                                <?php if($value['stock'] == '0') { 
-                                                echo '<h5>Out Of Stock</h5>'; 
-                                                }elseif($value['stock'] == '2'){
-                                                    echo '<h5>Out Of Season</h5>'; 
-                                                }?>
-                                                <div class="header">
+                                                    <?php if($value['stock'] == '0') { 
+                                                    echo '<h5>Out Of Stock</h5>'; 
+                                                    }elseif($value['stock'] == '2'){
+                                                        echo '<h5>Out Of Season</h5>'; 
+                                                    }?>
+                                                    <div class="header">
+                                                        <?php if($discountPercentage != 0) { ?> 
+                                                            <h5 class="card-title float-left"><?php echo round($discountPercentage)."% OFF"; ?></h5>
+                                                        <?php } ?>
+                                                        <!--<a href="#" class="add_to_favorite">
+                                                            <i class="fas fa-heart float-right"></i>
+                                                        </a>-->
+                                                    </div>
+                                                </div>
+                                                <a href="<?php echo base_url() . 'Cproduct/viewProduct/' . $value['ProductId']; ?>"> 
+                                                <img style="max-height: 145px;" class="img-fluid text-center" src="<?php echo base_url().$value['ProductImg']; ?>" alt="Card image cap">
+                                                </a>
+                                                <div class="product-info text-center">
+                                                    <p class="card-text product-card-inner-name" title="<?php echo $value['ProductName']; ?>"><?php echo $value['ProductName']; ?></p>
+                                                    <p class="card-text product-card-inner-weight">
+                                                        <?= empty($value['SaleUnitName']) ? $value['UnitName'] : $value['SaleUnitQty']. ' ' .$value['SaleUnitName'] ; ?></p>
+                                                    <p class="card-text product-card-inner-price d-inline"><script type="text/javascript">document.write(formatCurrency("<?php echo $value['SalePrice']; ?>",0)); </script></p>
                                                     <?php if($discountPercentage != 0) { ?> 
-                                                        <h5 class="card-title float-left"><?php echo round($discountPercentage)."% OFF"; ?></h5>
+                                                        <span class="product-discount"><del><script type="text/javascript">document.write(formatCurrency("<?php echo $value['Price']; ?>",0)); </script></del></span>
+                                                    <?php } 
+                                                    $productObject = (object) [
+                                                    'id' => $value['ProductId'],
+                                                    'pName' => $value['ProductName'],
+                                                    'price' => $value['SalePrice'],
+                                                    'img' => base_url().$value['ProductImg']
+                                                ];
+                                                ?>
+                                                <?php if($value['stock'] == '1') { ?>
+                                                    <div class="quantity-area d-flex justify-content-center align-items-center mt-2">
+                                                        <span class="d-inline-flex quantity-text mr-1">Qty</span>
+                                                        <input type="text" class="d-inline-flex quantity-input quantity">
+                                                        <span class="d-block quantity-button">
+                                                            <a href="javascript:void(0);" class="qty-pls d-block">+</a>
+                                                            <div class="separator"></div>
+                                                            <a href="javascript:void(0);" class="qty-mns d-block">-</a>
+                                                        </span>
+                                                    </div>
                                                     <?php } ?>
-                                                    <!--<a href="#" class="add_to_favorite">
-                                                        <i class="fas fa-heart float-right"></i>
-                                                    </a>-->
                                                 </div>
-                                            </div>
-                                            <img style="max-height: 145px;" class="img-fluid text-center" src="<?php echo base_url().$value['ProductImg']; ?>" alt="Card image cap">
-                                            <div class="product-info text-center">
-                                                <p class="card-text product-card-inner-name" title="<?php echo $value['ProductName']; ?>"><?php echo $value['ProductName']; ?></p>
-                                                <p class="card-text product-card-inner-weight">
-                                                    <?= empty($value['SaleUnitName']) ? $value['UnitName'] : $value['SaleUnitQty']. ' ' .$value['SaleUnitName'] ; ?></p>
-                                                <p class="card-text product-card-inner-price d-inline"><script type="text/javascript">document.write(formatCurrency("<?php echo $value['SalePrice']; ?>",0)); </script></p>
-                                                <?php if($discountPercentage != 0) { ?> 
-                                                    <span class="product-discount"><del><script type="text/javascript">document.write(formatCurrency("<?php echo $value['Price']; ?>",0)); </script></del></span>
-                                                <?php } 
-                                                $productObject = (object) [
-                                                'id' => $value['ProductId'],
-                                                'pName' => $value['ProductName'],
-                                                'price' => $value['SalePrice'],
-                                                'img' => base_url().$value['ProductImg']
-                                            ];
-                                            ?>
+                                                
+                                            
                                             <?php if($value['stock'] == '1') { ?>
-                                                <div class="quantity-area d-flex justify-content-center align-items-center mt-2">
-                                                    <span class="d-inline-flex quantity-text mr-1">Qty</span>
-                                                    <input type="text" class="d-inline-flex quantity-input quantity">
-                                                    <span class="d-block quantity-button">
-                                                        <a href="javascript:void(0);" class="qty-pls d-block">+</a>
-                                                        <div class="separator"></div>
-                                                        <a href="javascript:void(0);" class="qty-mns d-block">-</a>
-                                                    </span>
-                                                </div>
-                                                <?php } ?>
-                                            </div>
-                                        </a>
-                                        <?php if($value['stock'] == '1') { ?>
-                                        <a href="javascript:void(0);" class="product-card-btn mx-auto add-cart"
-                                        data-json="<?php echo htmlentities(json_encode($productObject), ENT_QUOTES, 'UTF-8'); ?>"
-                                        >Add to Cart</a>
-                                        <a href="javascript:void(0);" style="display: none;" class="product-card-btn mx-auto remove-cart"
-                                        data-json="<?php echo htmlentities(json_encode($productObject), ENT_QUOTES, 'UTF-8'); ?>"
-                                        >Remove From Cart</a>
-                                        <?php } ?>
-                                    </div>
+                                            <a href="javascript:void(0);" class="product-card-btn mx-auto add-cart"
+                                            data-json="<?php echo htmlentities(json_encode($productObject), ENT_QUOTES, 'UTF-8'); ?>"
+                                            >Add to Cart</a>
+                                            <a href="javascript:void(0);" style="display: none;" class="product-card-btn mx-auto remove-cart"
+                                            data-json="<?php echo htmlentities(json_encode($productObject), ENT_QUOTES, 'UTF-8'); ?>"
+                                            >Remove From Cart</a>
+                                            <?php } ?>
+                                        </div>
+                                    <!-- </a> -->
                                 </div>
+
+
                             </div>
                         <?php } ?>
                     </div>
@@ -260,6 +268,7 @@
                             cartTemplateCopy = cartTemplateCopy.replace(/{productId}/g, base_url + 'Cproduct/viewProduct/' + data[i].ProductId)
                             pjsonString = {id: data[i].ProductId, pName: data[i].ProductName, price: data[i].SalePrice, img: data[i].ProductImg};
                             cartTemplateCopy = cartTemplateCopy.replace(/{pjsonString}/g, data[i].Jsn);
+                            cartTemplateCopy = cartTemplateCopy.replace(/{detailPageLink}/g, base_url + 'Cproduct/viewProduct/' +  data[i].ProductId)
                             productArea.append(cartTemplateCopy);
                         }
                         if(data.length < perpage)
